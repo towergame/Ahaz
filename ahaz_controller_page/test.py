6d60cc3ad28c382fe2ccde4ff22eb64d8a957edc
towergame <tower@0x10.lv>
2026-02-05 21:57:57 +0200

    Resolve issue in ahaz_controller_page/test.py

--- ahaz_controller_page/test.py
+++ ahaz_controller_page/test.py
@@ -1,8 +1,10 @@
-import requests
 import json
-url="http://10.13.0.2:5000"
 
-namespace={"teamname":"1"}
-podresult = requests.get(url+"/get_pods_namespace",json=namespace)
-podresultlist=json.loads(podresult.text)
+import requests
+
+url = "http://10.13.0.2:5000"
+
+namespace = {"teamname": "1"}
+podresult = requests.get(url + "/get_pods_namespace", json=namespace)
+podresultlist = json.loads(podresult.text)
 print(podresult.text)
